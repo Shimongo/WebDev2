bd65324fcbb742f25c575a4137d69ab328f79018
Shimongo <Afifonmon@gmail.com>
2017-04-08 00:08:40 +0300

    Add files via upload

---
+++ ss/jScripts/JavaScript.js
@@ -0,0 +1,19 @@
+﻿var x = 0;
+var y = 0;
+function myFun() {
+    var a = parseFloat(document.getElementById('Text1').value);
+    x += a;
+    y++;
+    var z = parseFloat(x / y);
+    document.getElementById('avg').innerHTML = z;
+    document.getElementById('kamuth').innerHTML = y;
+
+}
+function myFun2()
+{
+    x = 0
+    y = 0
+    var z = 0
+    document.getElementById('avg').innerHTML = z;
+    document.getElementById('kamuth').innerHTML = y;
+}
